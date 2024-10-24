128b712a0fe8f3b2ba54cc93a711b2332a46efbb
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2024-10-24 19:02:04 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct Grammar;
+
+fn main() -> anyhow::Result<()> {
+    let parsed_data = Grammar::parse(Rule::field, "-273.15")?;
+    dbg!(parsed_data);
+
+    Ok(())
+}
